56f455d62c9918de2a9584280f02e04930b87852
DevRyanBush <106203316+DevRyanBush@users.noreply.github.com>
2022-07-10 15:31:47 -0600

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+function mouseOver(element, size){
+    element.style.color = "green";
+    element.style.fontSize = size;
+}
+function mouseOut(element, size){
+    element.style.color = "black";
+    element.style.fontSize = size;
+}
